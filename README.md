ec6cc05514bdfb0781b2c8eb747aa68855256c76
Rangerdevv <meerabdul2008@gmail.com>
2022-10-30 13:24:54 -0400

    bug fixes

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # RocketryDummy
+
+
+Citation:
+AOS Library:  https://michalsnik.github.io/aos/ 
+ Github: https://github.com/michalsnik/aos 
+
+Icons: https://iconify.design/ 
+
+Typewriter Library:  https://safi.me.uk/typewriterjs/ 
+Github: https://github.com/tameemsafi/typewriterjs 
+
+Twinkling Stars Background: https://archive.org/details/Stars_2D
+
+Planet Pictures: <a href="https://www.vecteezy.com/free-vector/planet">Planet Vectors by Vecteezy</a>
--- btn.js
+++ btn.js
@@ -2,35 +2,43 @@ let crd_ttl = document.getElementById("card_title")
 let crd_desc = document.getElementById("desc")
 let crd_btn_rght = document.getElementById("Card_right")
 let crd_btn_lft = document.getElementById("Card_left")
+let planetimg = document.getElementById("Img")
 
 let array = [
     {
         "Title": "Venus",
-        "Description": "Explore one of the hottest planet in our solar system."
+        "Description": "Explore one of the hottest planet in our solar system.",
+        "src":"./Image/Planet2-removebg-preview.png"
     },
     {
         "Title": "Earth",
-        "Description": "This is the description part 2"
+        "Description": "This is the description part 2",
+        "src":"./Image/Planet3-removebg-preview.png"
     },
     {
         "Title": "Mars",
-        "Description": "Explore one of the hottest planet in our solar system."
+        "Description": "Explore one of the hottest planet in our solar system.",
+        "src":"./Image/Planet4-removebg-preview.png"
     },
     {
         "Title": "Jupiter",
-        "Description": "Explore one of the hottest planet in our solar system."
+        "Description": "Explore one of the hottest planet in our solar system.",
+        "src":"./Image/Planet5-removebg-preview.png"
     },
     {
         "Title": "Saturn",
-        "Description": "Explore one of the hottest planet in our solar system."
+        "Description": "Explore one of the hottest planet in our solar system.",
+        "src":"./Image/Planet6-removebg-preview.png"
     },
     {
         "Title": "Uranus",
-        "Description": "Explore one of the hottest planet in our solar system."
+        "Description": "Explore one of the hottest planet in our solar system.",
+        "src":"./Image/Planet7-removebg-preview.png"
     },
     {
         "Title": "Neptune",
-        "Description": "Explore one of the hottest planet in our solar system."
+        "Description": "Explore one of the hottest planet in our solar system.",
+        "src":"./Image/Planet8-removebg-preview.png"
     }
 ]
 
@@ -43,6 +51,7 @@ crd_btn_rght.onclick = function(){
     }
     crd_ttl.innerText=array[counter].Title
     crd_desc.innerText=array[counter].Description
+    planetimg.src=array[counter].src
 }
 
 crd_btn_lft.onclick = function(){
@@ -52,11 +61,13 @@ crd_btn_lft.onclick = function(){
     }
     crd_ttl.innerText=array[counter].Title
     crd_desc.innerText=array[counter].Description
+    planetimg.src=array[counter].src
 }
 
 function loadtxt() {
     crd_ttl.innerText=array[counter].Title
     crd_desc.innerText=array[counter].Description
+    planetimg.src=array[counter].src
 }
 
 window.onload(loadtxt())
