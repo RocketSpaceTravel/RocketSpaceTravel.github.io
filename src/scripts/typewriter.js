c16cf00addf53dd7a4c9bf6583bc48b2bf9d0908
Rangerdevv <meerabdul2008@gmail.com>
2022-11-08 19:10:07 -0500

    redoing the website

--- src/scripts/typewriter.js
+++ src/scripts/typewriter.js
@@ -7,18 +7,18 @@ var typewriter = new Typewriter(app, {
 
 typewriter
   .pauseFor(1000)
-  .typeString('Making space travel <span style="color: #9390FF;">convenient.</span>')
+  .typeString('Making space travel <span style="color: #5ec183;">convenient.</span>')
   .pauseFor(300)
   .deleteChars(11)
-  .typeString('<span style="color: #9390FF;">faster.</span>')
+  .typeString('<span style="color: #5ec183;">faster.</span>')
   .pauseFor(1000)
   .deleteChars(8)
-  .typeString('<span style="color: #9390FF;">easier.</span>')
+  .typeString('<span style="color: #5ec183;">easier.</span>')
   .pauseFor(1000)
   .deleteChars(7)
-  .typeString('<span style="color: #9390FF;">enjoyable.</span>')
+  .typeString('<span style="color: #5ec183;">enjoyable.</span>')
   .pauseFor(1000)
   .deleteChars(11)
-  .typeString('<span style="color: #9390FF;">educating.</span>')
+  .typeString('<span style="color: #5ec183;">educating.</span>')
   .pauseFor(1000)
   .start();
