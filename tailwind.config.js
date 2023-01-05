cd7c60dbd5fd07c22ed852229fcc0b623c34b560
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-01-05 14:56:56 +0000

    Some small Credit changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./about.html", "./pricing.html", "./credits.html"],
+  content: ["./index.html", "./about.html", "./pricing.html", "./Documentation.html"],
   theme: {
     extend: {},
     screens: {
